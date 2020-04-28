44fe7af7e69d14afa23cdcd6b8c59a8d9d183d5a
Johan Hattne <hattne@ucla.edu>
2020-04-28 00:28:45 -0700

    Use provideInfo() from CookieSessionProvider

--- includes/WPMWSessionProvider.php
+++ includes/WPMWSessionProvider.php
@@ -51,13 +51,101 @@ class WPMWSessionProvider extends CookieSessionProvider {
     }
 
 
+    public function parent_provideInfo( WebRequest $request ) {
+        $sessionId = $this->getCookie(
+            $request, $this->params['sessionName'], '' );
+        $info = [
+            'provider' => $this,
+            'forceHTTPS' => $this->getCookie(
+                $request, 'forceHTTPS', '', false )
+        ];
+        if ( SessionManager::validateSessionId( $sessionId ) ) {
+            $info['id'] = $sessionId;
+            $info['persisted'] = true;
+        }
+
+        list( $userId, $userName, $token ) = $this->getUserInfoFromCookies(
+            $request );
+#        list( $userId, $userName, $token ) = parent::getUserInfoFromCookies(
+#            $request );
+        if ( $userId !== null ) {
+            try {
+                $userInfo = UserInfo::newFromId( $userId );
+            } catch ( \InvalidArgumentException $ex ) {
+                return null;
+            }
+
+            // Sanity check
+            if ( $userName !== null && $userInfo->getName() !== $userName ) {
+                $this->logger->warning(
+                    'Session "{session}" requested with mismatched UserID and UserName cookies.',
+                    [
+                        'session' => $sessionId,
+                        'mismatch' => [
+                            'userid' => $userId,
+                            'cookie_username' => $userName,
+                            'username' => $userInfo->getName(),
+                        ],
+                    ] );
+                return null;
+            }
+
+            if ( $token !== null ) {
+                if ( !hash_equals( $userInfo->getToken(), $token ) ) {
+                    $this->logger->warning(
+                        'Session "{session}" requested with invalid Token cookie.',
+                        [
+                            'session' => $sessionId,
+                            'userid' => $userId,
+                            'username' => $userInfo->getName(),
+                        ] );
+                    return null;
+                }
+                $info['userInfo'] = $userInfo->verified();
+                $info['persisted'] = true; // If we have user+token, it should be
+            } elseif ( isset( $info['id'] ) ) {
+                $info['userInfo'] = $userInfo;
+            } else {
+                // No point in returning, loadSessionInfoFromStore() will
+                // reject it anyway.
+                return null;
+            }
+        } elseif ( isset( $info['id'] ) ) {
+            // No UserID cookie, so insist that the session is anonymous.
+            // Note: this event occurs for several normal activities:
+            // * anon visits Special:UserLogin
+            // * anon browsing after seeing Special:UserLogin
+            // * anon browsing after edit or preview
+            $this->logger->debug(
+                'Session "{session}" requested without UserID cookie',
+                [
+                    'session' => $info['id'],
+                ] );
+            $info['userInfo'] = UserInfo::newAnonymous();
+        } else {
+            // No session ID and no user is the same as an empty session, so
+            // there's no point.
+            return null;
+        }
+
+        return $info;
+    }
+
+
     public function provideSessionInfo( WebRequest $request ) {
 
-        $this->logger->info( "MARKER: Here we go" );
+        $this->logger->info( "MARKER: Here we go at " . $this->priority );
 
 
         // Returns null when nobody logged in.
-        $sessionInfo = parent::provideSessionInfo( $request );
+        $info = $this->parent_provideInfo( $request );
+
+        if ($info) {
+            $sessionInfo = new SessionInfo( $this->priority, $info );
+        } else {
+            $sessionInfo = null;
+        }
+
 
         if ( $sessionInfo === null ) {
             $this->logger->info( "MARKER: sessionInfo is null" );
@@ -101,9 +189,7 @@ class WPMWSessionProvider extends CookieSessionProvider {
                     'userInfo' => $userInfo, // XXX what about $userInfo->verified()
                     'persisted' => true, // XXX was false, or rather $persisted,
 #                    'forceUse' => $forceUse, // XXX was true, or rather $forceUse
-            ] );
-
-
+                ] );
             }
 
         } else {
@@ -211,9 +297,10 @@ class WPMWSessionProvider extends CookieSessionProvider {
 
 #        $user = $userInfo->getUser();
 #        foreach ( $user->getGroups() as $t ) {
-#            $this->logger->info( "MARKER: See a thing of " . $t . " userID is " . $user->getId());
+#            $this->logger->info(
+#                "MARKER: See a thing of " . $t .
+#                " userID is " . $user->getId() );
 #        }
-
 
         return new SessionInfo( SessionInfo::MAX_PRIORITY, [
             'provider' => $this,
@@ -224,14 +311,13 @@ class WPMWSessionProvider extends CookieSessionProvider {
         ] );
     }
 
+
     // XXX Defining this empty re-enables the logout link, but why?
     // Then, logging out will kill the MediaWiki session, but not the
     // WordPress ditto.
-
-
-    // XXX Only now notice: "Logging out is not possible when using
-    // WPMWSessionProvider sessions"
 /*
+    // XXX Only now notice: "Logging out is not possible when using
+    // WPMWSessionProider sessions"
     public function refreshSessionInfo(
         SessionInfo $info, WebRequest $request, &$metadata ) {
         $this->logger->info( "MARKER: in refreshSessionInfo()" );
@@ -283,10 +369,10 @@ class WPMWSessionProvider extends CookieSessionProvider {
             }
         );
         return true;
-*/
 
         return true;
     }
+*/
 
 
     // XXX Stuff for testing below
@@ -300,8 +386,10 @@ class WPMWSessionProvider extends CookieSessionProvider {
         return parent::unpersistSession( $request );
     }
 
+
     public function setLoggedOutCookie( $loggedOut, WebRequest $request ) {
-        // called before displaying the login-stuff when nobody is logged in
+        // called before displaying the login-stuff when nobody is
+        // logged in
         $this->logger->info( "MARKER: setLoggedOutCookie()" );
         return parent::setLoggedOutCookie( $loggedOut, $request );
     }
