6a53669417551666d2948b27f9eb753fe55b1729
Johan Hattne <hattne@ucla.edu>
2020-09-24 01:47:45 -0700

    Revert to bootstrapping with wp-load.php only and send email notification from WordPress when new users are inserted Also remove the cli guard because these are classes and remove the old AuthWP.php code, because it is no longer used.

--- includes/WPMWSessionProvider.php
+++ includes/WPMWSessionProvider.php
@@ -23,32 +23,16 @@ use MediaWiki\Session\SessionInfo;
 use MediaWiki\Session\UserInfo;
 
 
-// Bootstrap WordPress.  This seems rather foolish since surely the
-// names of things are bound to clash somewhere, but we want to be
-// able to handle everything as if WordPress was doing it natively
-// including respecting any plugins that might be in place.
-if ( php_sapi_name() !== 'cli' ) {
-    // Relative path to WordPress installation.  In the default '..'
-    // MediaWiki is installed in a 'wiki' directory off the main
-    // WordPress root.
-    $WP_relpath = MediaWikiServices::getInstance()
-                 ->getConfigFactory()
-                 ->makeConfig( 'WPMW' )
-                 ->get( 'WPMWPath' );
-    require_once $WP_relpath
-        . DIRECTORY_SEPARATOR . 'wp-load.php';
-    require_once $WP_relpath
-        . DIRECTORY_SEPARATOR . 'wp-includes'
-        . DIRECTORY_SEPARATOR . 'registration.php';
-}
+// Bootstrap WordPress using the relative path to its installation
+// directory.  The default value, '..', implies that MediaWiki is
+// installed in a directory next to WordPress's wp-load.php.
+$WP_relpath = MediaWikiServices::getInstance()
+            ->getConfigFactory()
+            ->makeConfig( 'WPMW' )
+            ->get( 'WPMWPath' );
+require_once $WP_relpath . DIRECTORY_SEPARATOR . 'wp-load.php';
 
 
-
-/**
- * A MediaWiki extension to delegate authentication and user
- * management to a local WordPress installation.  See
- * http://ciarang.com/wiki/page/WPMW for more information.
- */
 class WPMWSessionProvider extends ImmutableSessionProviderWithCookie {
     public function __construct( $params = [] ) {
         parent::__construct( $params );
@@ -72,7 +56,7 @@ class WPMWSessionProvider extends ImmutableSessionProviderWithCookie {
 
 
     // Log out user from WordPress early, possibly before MediaWiki
-    // completes logout.
+    // logout completes.
     public static function onUserLogout( &$user ) {
         wp_logout();
         return true;
@@ -85,9 +69,9 @@ class WPMWSessionProvider extends ImmutableSessionProviderWithCookie {
     // automatically signed in to MediaWiki.
     public function provideSessionInfo( WebRequest $request ) {
         // Get the canonical name corresponding to the user logged in
-        // through WordPress.  If no WordPress user is logged in, a
+        // to WordPress.  If there is no logged-in WordPress user, a
         // downstream session provider will have to provide the
-        // session information.
+        // session information instead.
         $wp_user = wp_get_current_user();
         if ( !$wp_user->exists() ) {
             return null;
@@ -97,12 +81,11 @@ class WPMWSessionProvider extends ImmutableSessionProviderWithCookie {
 
 
         // Clear the UserID cookie if the corresponding username
-        // matches the name of the user logged in through WordPress.
-        // This will prevent downstream session providers from
-        // validating the session after the user logs out from
-        // WordPress.  Do not clear the UserName cookie, because it is
-        // used to populate the "Username" field on MediaWiki's "Log
-        // in" page.
+        // matches the name of the logged-in WordPress user.  This
+        // will prevent downstream session providers from validating
+        // the sessions of users after they log out from WordPress.
+        // Do not clear the UserName cookie, because it is used to
+        // populate the "Username" field on MediaWiki's "Log in" page.
         $userID = $request->getCookie( 'UserID' );
         if ( $userID !== null ) {
             $userInfo = UserInfo::newFromId( $userID );
